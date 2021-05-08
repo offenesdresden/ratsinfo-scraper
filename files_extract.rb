dbea3dc5ddb4d21b7cb8df15979c666d26c8bbbe
Astro <astro@spaceboyz.net>
2021-05-09 00:45:20 +0200

    files_extract: add for pdf download

---
+++ files_extract.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env/ruby
+
+require 'open-uri'
+require 'json'
+require 'fileutils'
+require 'socket'
+require './dresden_dns_hack'
+
+DIR = "data/files"
+Dir.foreach(DIR) do |json_name|
+  next unless json_name =~ /\.json$/
+
+  json_path = "#{DIR}/#{json_name}"
+  pdf_path = json_path.sub(/\.json$/, ".pdf")
+  json = JSON.load File.open(json_path)
+
+  if json['mimeType'] == "application/pdf"
+    pdf_url = json['downloadUrl']
+    puts "GET " + pdf_url
+    open pdf_url do |res|
+      code = res.status[0]
+      raise "HTTP #{code}" if code != "200"
+
+      FileUtils.cp res.path, pdf_path
+    end
+  else
+    puts "Unrecognized MIME type: #{json['mimeType']}"
+  end
+rescue OpenURI::HTTPError => e
+  puts e.message || e
+end
--- scrape.rb
+++ scrape.rb
@@ -4,18 +4,8 @@ require 'open-uri'
 require 'json'
 require 'socket'
 require 'fileutils'
+require './dresden_dns_hack'
 
-# HACK around missing DNS record
-class TCPSocket
-  alias :initialize_old :initialize
-
-  def initialize(host=nil, port=0, local_host=nil, local_port=nil)
-    # fix broken DNS
-    host = "194.49.19.58" if host == "oparl.dresden.de"
-
-    initialize_old(host, port, local_host, local_port)
-  end
-end
 
 # HTTP/JSON helper
 module Scrape
