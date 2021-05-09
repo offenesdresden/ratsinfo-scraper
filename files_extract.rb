8ab8e15f5b7287c88650f5628ae979275a0de0ec
Astro <astro@spaceboyz.net>
2021-05-09 22:47:25 +0200

    files_extract.rb: run pdftotext

--- files_extract.rb
+++ files_extract.rb
@@ -15,21 +15,26 @@ Dir.foreach(DIR) do |json_name|
 
   if json['mimeType'] == "application/pdf"
     pdf_path = json_path.sub(/\.json$/, ".pdf")
-    next if File.exist? pdf_path
+    txt_path = json_path.sub(/\.json$/, ".txt")
+    next if File.exist? txt_path
 
-    pdf_url = json['downloadUrl'].sub(/^http:/, "https:")
-    puts "GET " + pdf_url
-    begin
-      open pdf_url do |res|
-        code = res.status[0]
-        raise "HTTP #{code}" if code != "200"
+    unless File.exist? pdf_path
+      pdf_url = json['downloadUrl'].sub(/^http:/, "https:")
+      puts "GET " + pdf_url
+      begin
+        open pdf_url do |res|
+          code = res.status[0]
+          raise "HTTP #{code}" if code != "200"
 
-        IO.copy_stream res, "#{pdf_path}.tmp"
-        FileUtils.mv "#{pdf_path}.tmp", pdf_path
+          IO.copy_stream res, "#{pdf_path}.tmp"
+          FileUtils.mv "#{pdf_path}.tmp", pdf_path
+        end
+      rescue OpenURI::HTTPError => e
+        puts e
+        next
       end
-    rescue OpenURI::HTTPError => e
-      puts e
     end
+    print `pdftotext -enc UTF-8 #{pdf_path} && rm #{pdf_path}`
   else
     puts "Unrecognized MIME type: #{json['mimeType']}"
   end
